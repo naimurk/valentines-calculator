92fa42bbee5cd7ba71caa16701ec14ba92553940
naimurk <naimurk781@gmail.com>
2023-02-16 17:26:57 +0600

    four

--- js/valen.js
+++ js/valen.js
@@ -2,6 +2,7 @@ document.getElementById('kitkat-btn').addEventListener('click', function(){
     const kitkatfield = document.getElementById('kitkat-field')
     const kitkatvalue = kitkatfield.value;
     const sum = 100*kitkatvalue
+    kitkatfield.value = ''
    
     
     const choclateField = document.getElementById('kitkat-price')
@@ -10,27 +11,31 @@ document.getElementById('kitkat-btn').addEventListener('click', function(){
     choclateField.innerText = choclateValue + sum;
 
 })
+
 document.getElementById('diary-btn').addEventListener('click', function(){
     const diaryfield = document.getElementById('diary-field')
     const diaryvalue = diaryfield.value;
     const sum = 50*diaryvalue
+    diaryfield.value = ''
    
     
     const diaryFieldPrice = document.getElementById('diary-price')
     const diaryValueString = diaryFieldPrice.innerText
-    const diaryValue = parseFloat(diaryValueString);
-    diaryFieldPrice.innerText = diaryValue + sum;
+    const diaryValuePrice = parseFloat(diaryValueString);
+    diaryFieldPrice.innerText = diaryValuePrice + sum;
 
 })
-document.getElementById('kitkat-btn').addEventListener('click', function(){
-    const kitkatfield = document.getElementById('kitkat-field')
-    const kitkatvalue = kitkatfield.value;
-    const sum = 100*kitkatvalue
+
+document.getElementById('rose-btn').addEventListener('click', function(){
+    const rosefield = document.getElementById('rose-field')
+    const rosevalue = rosefield.value;
+    const sum = 150*rosevalue
+    rosefield.value = ''
    
     
-    const choclateField = document.getElementById('kitkat-price')
-    const choclateValueString = choclateField.innerText
-    const choclateValue = parseFloat(choclateValueString);
-    choclateField.innerText = choclateValue + sum;
+    const roseFieldprice = document.getElementById('rose-price')
+    const roseValueString = roseFieldprice.innerText
+    const roseValueprice = parseFloat(roseValueString);
+    roseFieldprice.innerText = roseValueprice + sum;
 
 })
