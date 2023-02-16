47b8259f09b18b034f58108e6ffdcc730684d69f
naimurk <naimurk781@gmail.com>
2023-02-16 17:10:05 +0600

    third

--- js/valen.js
+++ js/valen.js
@@ -1,1 +1,36 @@
-console.log('hello world');
+document.getElementById('kitkat-btn').addEventListener('click', function(){
+    const kitkatfield = document.getElementById('kitkat-field')
+    const kitkatvalue = kitkatfield.value;
+    const sum = 100*kitkatvalue
+   
+    
+    const choclateField = document.getElementById('kitkat-price')
+    const choclateValueString = choclateField.innerText
+    const choclateValue = parseFloat(choclateValueString);
+    choclateField.innerText = choclateValue + sum;
+
+})
+document.getElementById('diary-btn').addEventListener('click', function(){
+    const diaryfield = document.getElementById('diary-field')
+    const diaryvalue = diaryfield.value;
+    const sum = 50*diaryvalue
+   
+    
+    const diaryFieldPrice = document.getElementById('diary-price')
+    const diaryValueString = diaryFieldPrice.innerText
+    const diaryValue = parseFloat(diaryValueString);
+    diaryFieldPrice.innerText = diaryValue + sum;
+
+})
+document.getElementById('kitkat-btn').addEventListener('click', function(){
+    const kitkatfield = document.getElementById('kitkat-field')
+    const kitkatvalue = kitkatfield.value;
+    const sum = 100*kitkatvalue
+   
+    
+    const choclateField = document.getElementById('kitkat-price')
+    const choclateValueString = choclateField.innerText
+    const choclateValue = parseFloat(choclateValueString);
+    choclateField.innerText = choclateValue + sum;
+
+})
